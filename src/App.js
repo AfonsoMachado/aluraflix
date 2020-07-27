ed6884d7ed538c07ad93d0ac1dd57c9d3a6182a5
Afonso Machado <afonso777@hotmail.com>
2020-07-27 12:57:08 -0300

    iniciando tags e assets

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Menu from './components/Menu/index';
 
 function App() {
   return (
     <div>
-
+      <Menu />
+      <h1>Hello World</h1>
     </div>
   );
 }
